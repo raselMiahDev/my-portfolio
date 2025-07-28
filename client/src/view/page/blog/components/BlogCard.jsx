16669d76e8658e8d8e5abba28e9c6b4234103d15
Rasel <raselmiahdev@gmail.com>
2025-07-28 22:54:04 +0600

    redeploy

---
+++ client/src/view/page/blog/components/BlogCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogCard = () => {
+    return (
+        <div>
+            block card 
+        </div>
+    );
+};
+
+export default BlogCard;
--- client/src/view/page/home-page/Home.jsx
+++ client/src/view/page/home-page/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "./Hero"
 import Contact from "../../components/contact/Contact";
 import Project from "../../components/project/Project";
 import Skills from "../../components/skills/Skills";
+import BlogPage from "../blog/blog-page";
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Hero/>
             <Project/>
             <Skills/>
+            <BlogPage/>
             <Contact/>
         </>
     );
